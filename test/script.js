b01b8c82391684dd4a64a6ddb1b5f7706979e76f
arun <arun@opsshield.com>
2026-01-06 20:27:44 +0530

    first script

---
+++ test/script.js
@@ -0,0 +1,7 @@
+console.log('Downloading content...');
+console.log('Progress: 0%');
+console.log('Progress: 25%');
+console.log('Progress: 50%');
+console.log('Progress: 75%');
+console.log('Progress: 100%');
+console.log('Download complete!');
